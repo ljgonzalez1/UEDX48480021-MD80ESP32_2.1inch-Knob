70c220bdca591eb51e3a05159f6d334428f4151c
Luis González <ljgonzalez@uc.cl>
2025-08-14 20:35:35 -0400

    Fixes

--- Libraries/ui/src/screens/ui_working.c
+++ Libraries/ui/src/screens/ui_working.c
@@ -104,7 +104,7 @@ void ui_working_screen_init(void)
     lv_obj_set_y(ui_Label1, 155);
     lv_obj_set_align(ui_Label1, LV_ALIGN_CENTER);
     lv_label_set_text(ui_Label1, "stop");
-    lv_obj_set_style_text_font(ui_Label1, &lv_font_montserrat_40, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_font(ui_Label1, &lv_font_montserrat_14, LV_PART_MAIN | LV_STATE_DEFAULT);
 
     ui_Label2 = lv_label_create(ui_working);
     lv_obj_set_width(ui_Label2, LV_SIZE_CONTENT);   /// 1
@@ -113,14 +113,14 @@ void ui_working_screen_init(void)
     lv_obj_set_y(ui_Label2, -83);
     lv_obj_set_align(ui_Label2, LV_ALIGN_CENTER);
     lv_label_set_text(ui_Label2, "Working");
-    lv_obj_set_style_text_font(ui_Label2, &lv_font_montserrat_40, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_font(ui_Label2, &lv_font_montserrat_14, LV_PART_MAIN | LV_STATE_DEFAULT);
 
     ui_Label3 = lv_label_create(ui_working);
     lv_obj_set_width(ui_Label3, LV_SIZE_CONTENT);   /// 1
     lv_obj_set_height(ui_Label3, LV_SIZE_CONTENT);    /// 1
     lv_obj_set_align(ui_Label3, LV_ALIGN_CENTER);
     lv_label_set_text(ui_Label3, "15");
-    lv_obj_set_style_text_font(ui_Label3, &lv_font_montserrat_40, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_font(ui_Label3, &lv_font_montserrat_14, LV_PART_MAIN | LV_STATE_DEFAULT);
 
     bar_time=lv_timer_create(bar_update_cb, 1000, NULL); // 每秒调用一次bar_update_cb函数
 
